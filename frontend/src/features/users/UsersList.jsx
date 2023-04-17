9d059d8fa7c49cc586161a0e0e70c1d933841de4
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-04-18 01:56:20 +0530

    redux

---
+++ frontend/src/features/users/UsersList.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useGetUsersQuery } from "./usersApiSlice";
+
+const UsersList = () => {
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery();
+
+  let content;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (isError) {
+    content = (
+      <p className={isError ? "errmsg" : "offscreen"}> {error?.data.message}</p>
+    );
+  }
+
+  if (isSuccess) {
+    const { ids } = users;
+
+    const tableContent = ids?.length
+      ? ids.map((userId) => <User key={userId} userId={userId} />)
+      : null;
+
+    content = (
+      <table className="table--users table">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th user__username">
+              Username
+            </th>
+            <th scope="col" className="table__th user__roles">
+              Roles
+            </th>
+            <th scope="col" className="table__th user__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+
+  return content;
+};
+
+export default UsersList;
